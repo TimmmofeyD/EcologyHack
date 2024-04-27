97340dae49cfb5022e4789093c193a9776d0cb41
d1str1be <d1str1be@vk.com>
2024-04-28 01:52:08 +0300

    Again fixes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from sensor import Sensor
 
 
-#Select port for serial connection
+# Select port for serial connection
 port = input("Enter serial port (a.e. COM5): ")
-#init air dirtiness sensor
+# init air dirtiness sensor
 sds011 = Sensor(port)
-#infinite loop for sensor scan and information print
+# infinite loop for sensor scan and information print
 while True:
-    sds011.print(sds011.get_sensor_data())
+    sds011.print_data(sds011.get_sensor_data())
--- app/sensor.py
+++ app/sensor.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
 from serial import Serial, EIGHTBITS, STOPBITS_ONE, PARITY_NONE
-import time, struct
+import time
+import struct
+
 
 class Sensor:
 
     def __init__(self, port):
-        self.port = port # Set this to your serial port.
+        self.port = port  # Set this to your serial port.
         self.baudrate = 9600
 
         # Prepare serial connection.
-        self.ser = Serial(port, baudrate=self.baudrate, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE)
+        self.ser = Serial(port, baudrate=self.baudrate, bytesize=EIGHTBITS,
+                          parity=PARITY_NONE, stopbits=STOPBITS_ONE)
         self.ser.flushInput()
 
         self.HEADER_BYTE = b"\xAA"
@@ -18,41 +21,41 @@ class Sensor:
 
         self.byte, self.previousbyte = b"\x00", b"\x00"
 
-
     def get_sensor_data(self):
         self.previousbyte = self.byte
         self.byte = self.ser.read(size=1)
-        #print(byte)
-        
+        # print(byte)
+
         # We got a valid packet header.
         if self.previousbyte == self.HEADER_BYTE and self.byte == self.COMMANDER_BYTE:
-            packet = self.ser.read(size=8) # Read 8 more bytes
-            #print(packet)
-            
+            packet = self.ser.read(size=8)  # Read 8 more bytes
+            # print(packet)
+
             # Decode the packet - little endian, 2 shorts for pm2.5 and pm10, 2 ID bytes, checksum.
             readings = struct.unpack('<HHcccc', packet)
-            
+
             # Measurements.µg/m³
             pm_25 = readings[0]/10.0
             pm_10 = readings[1]/10.0
-            
+
             # ID
-            id = packet[4:6]
-            id_bytes=bytes(id).hex()
-            #print(id)
-            
+            sensor_id = packet[4:6]
+            id_bytes = bytes(sensor_id).hex()
+            # print(id)
+
             # Prepare checksums.
             checksum = readings[4][0]
             calculated_checksum = sum(packet[:6]) & 0xFF
             checksum_verified = (calculated_checksum == checksum)
-            #print(checksum_verified)
-            
+            # print(checksum_verified)
+
             # Message tail.
             tail = readings[5]
-            
+
             if tail == self.TAIL_BYTE and checksum_verified:
-                #print("PM 2.5:", pm_25, "μg/m^3  PM 10:", pm_10, "μg/m^3")
+                # print("PM 2.5:", pm_25, "μg/m^3  PM 10:", pm_10, "μg/m^3")
                 return pm_25, pm_10, id_bytes
-            
-    def print(pm_25="NaN", pm_10="NaN", id_bytes="NaN"):
-        print("PM 2.5:", pm_25, "μg/m^3  PM 10:", pm_10, "μg/m^3", "ID:", id_bytes)
+
+    def print_data(self, pm_25="NaN", pm_10="NaN", id_bytes="NaN"):
+        print("PM 2.5:", pm_25, "μg/m^3  PM 10:",
+              pm_10, "μg/m^3", "ID:", id_bytes)
